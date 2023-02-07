471b30db103a9570facd153ab2d3b72e59753207
Lucas Dimer Justo <37344488+LucasJusto@users.noreply.github.com>
2023-02-07 13:38:17 -0300

    Project initiation

---
+++ MovieDBRes/MovieDBRes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDBRes
+//
+//  Created by Lucas Justo on 07/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
